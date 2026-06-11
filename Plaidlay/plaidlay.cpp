da1b76036ee8f5f7582151ac80ccf5e9a67eaffd
Peeda <107806301+Peeda@users.noreply.github.com>
2026-06-11 11:29:18 -0400

    skeleton code, nix stuff

---
+++ Plaidlay/plaidlay.cpp
@@ -0,0 +1,10 @@
+// okay this is just a temp file to get some code going
+// std::function has overhead but it's whatever for now
+
+template <typename T>
+class naiveSeq {
+private:
+	std::vector<T> data;
+public:
+
+};
